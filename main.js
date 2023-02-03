579673edfd297a5ee045c8dea6ededc2a4ac200e
shioko <ichisadashioko@gmail.com>
2023-02-04 02:51:31 +0900

    create basic GUI application

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+var browser_window;
+
+function createWindow() {
+    browser_window = new BrowserWindow({
+        width: 1600,
+        height: 900,
+        webPreferences: {
+            plugins: true,
+            contextIsolation: false,
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            enableRemoteModule: true,
+            backgroundThrottling: false,
+            nativeWindowOpen: true,
+        },
+    });
+
+    browser_window.loadFile('index.html');
+
+    // const contents = browser_window.webContents;
+    // console.log(contents);
+}
+
+app.whenReady().then(() => {
+    createWindow()
+});
+
+// quitting the app when no windows are open on non-macOS platforms
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') { app.quit(); }
+});
